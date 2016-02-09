59fa1c1f0ef28fdf6629ac39c226a5dafe2c841b
Henderson, Jonathan (GE Global Research) <jonathan.henderson@ge.com>
2016-02-08 16:15:41 -0800

    Added demo webapp

---
+++ demo-webapp/client/assets/js/app.js
@@ -0,0 +1,42 @@
+(function() {
+  'use strict';
+
+  angular.module('application', [
+    'ui.router',
+    'ngAnimate',
+
+    //foundation
+    'foundation',
+    'foundation.dynamicRouting',
+    'foundation.dynamicRouting.animations'
+  ]).controller('singleLocationController',
+    function($scope, $http){
+      $scope.location = "Test";
+      $http.get('http://pmapi/singlelocation').then(function(response){
+        console.log("singlelocation response: ",response);
+        $scope.data = response.data;
+      });
+
+  })
+    .config(config)
+    .run(run)
+  ;
+
+  config.$inject = ['$urlRouterProvider', '$locationProvider'];
+
+  function config($urlProvider, $locationProvider) {
+    $urlProvider.otherwise('/');
+
+    $locationProvider.html5Mode({
+      enabled:false,
+      requireBase: false
+    });
+
+    $locationProvider.hashPrefix('!');
+  }
+
+  function run() {
+    FastClick.attach(document.body);
+  }
+
+})();
